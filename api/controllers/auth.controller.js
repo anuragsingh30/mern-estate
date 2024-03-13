1e100bff3abfd5a0f05e52ccfba193654e00134e
anurag singh <21052143@kiit.ac.in>
2024-03-13 20:56:57 +0530

    created middlewaare

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,17 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup=async (req,res)=>{
+export const signup=async (req,res,next)=>{
     const {username,email,password} = req.body;
     const hashedPassword =bcryptjs.hashSync(password,10);
     const newUser =new User({username,email,password: hashedPassword});
-    await newUser.save();
+    try{
+        await newUser.save();
+        res.status(201).json('User created successfully');
+    } catch(error){
+        next(errorHandler(550,'error form the funtion'));
+    }
+    
     res.status(201).json('User created sccessfully');
 };
